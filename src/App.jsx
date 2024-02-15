0e001aacf98e601e20a6fd3f43eedf426f97aa6e
Pushkar S <spushkar0071234@gmail.com>
2024-02-15 22:16:18 +0530

    Refactoring to merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
-import Home from './Home';
-import Login from './Login';
+import Home from './components/Home';
+import Login from './components/Login';
 import './App.css';
 
 function App() {
